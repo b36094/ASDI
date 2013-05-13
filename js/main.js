0418ff6433fbb6c5f6bdca2eafa89b6f666cdf39
Nactus Berrilli <nactus@fabiana.com>
2013-05-12 21:36:37 -0700

    jqm logic added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,78 @@
 // Name: Andrei Birsan
 // Course month: 05.2013
 // File Purpose: Basic Start-up Template
+
+//#homePage starts here
+$(document).on('pageinit','#homePage', function() {
+	
+});//here ends #homePage
+
+//#newsFeed starts here
+$(document).on('pageinit', '#newsFeed', function(){
+	
+});//here ends #newsFeed
+
+//#aboutPage starts here
+$(document).on('pageinit', '#aboutPage', function(){
+	
+});//here ends #aboutPage
+
+//#detailsPage starts here
+$(document).on('pageinit', '#detailsPage', function(){
+	
+});//here ends #detailsPage
+
+//#newEntryPage starts here
+$(document).on('pageinit', '#newEntryPage', function(){
+
+	//function to parse the form
+	var $myFirstForm = $('#firstForm');
+	
+	//access the validation property (plugging)
+	$myFirstForm.validate({
+		//ignores the items that have the class "ignore" on them			
+		ignore: ".ignore",
+		invalidHandler: function() {},		
+		submitHandler: function(randomId){
+		
+		//serialize the form data into an object 
+		var data = $myFirstForm.serializeArray();
+		
+		//stringify the data from the form object
+		var jsonObj = JSON.stringify(data);
+		
+		//add a random number for the key
+		var randomId = genRandomId();
+		
+		//add the string conversion to the localStorage with a key-value
+		localStorage.setItem(randomId,jsonObj);
+		
+		//reset the form after localStorage insertion
+		$($myFirstForm)[0].reset();
+		
+		//refresh localStorage
+		window.location = '#homePage';
+				
+		}
+	});
+		
+	
+	
+});//here ends #newEntryPage
+
+//Refresh the '#displayData' page to update all the local store changes
+	$('#submitBt').click(function(){
+		
+		alert("I've been cliked");
+		//tranports to displayData
+		window.location = '#homePage';	
+		
+		//refreshes displayData
+		window.location.reload('#homePage');
+});
+
+//genRandomId function creates a random number and returns it to be caught by the validator 
+var genRandomId = function(){
+	var randomId = Math.floor(Math.random() * 10000001);
+	return randomId;
+};
